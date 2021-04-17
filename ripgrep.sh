0b3cd39aea031869e3f5b3887e1f9bee81d36d14
sittingfrog <jvj.norton@gmail.com>
2021-04-17 15:47:28 -0400

    Update ripgrep.sh

--- ripgrep.sh
+++ ripgrep.sh
@@ -1,4 +1,4 @@
- #!/bin/bash 
+#!/bin/bash 
 # install ripgrep tool. this can be used to search code for all occurrences of a string in a directory.
 # ripgrep is a combination of grep and silversearcher functionality
 
