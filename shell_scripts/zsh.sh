a21ec87b1710953027bd9335f3cd9cdb998fef15
sittingfrog <jvj.norton@gmail.com>
2021-04-17 19:28:25 -0400

    Update zsh.sh

--- shell_scripts/zsh.sh
+++ shell_scripts/zsh.sh
@@ -2,3 +2,5 @@
 
 brew install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+mv ~/.zshrc ~/.zshrc_bak
+cp ./assets/.zshrc ~/.zshrc
