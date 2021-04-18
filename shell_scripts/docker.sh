79559070c1b75c61c378f8354cb5ff6d9a8044a2
sittingfrog <jvj.norton@gmail.com>
2021-04-18 17:24:24 -0400

    Update docker.sh

--- shell_scripts/docker.sh
+++ shell_scripts/docker.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# Cask is used because it handels docker setup coding that is otherwise required (as of 20210418)
+# https://medium.com/crowdbotics/a-complete-one-by-one-guide-to-install-docker-on-your-mac-os-using-homebrew-e818eb4cfc3
+
 brew install --cask docker
