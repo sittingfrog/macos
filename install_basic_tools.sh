0351d434b62ef4e3e3824ae1f6d29de0ac3ae9b4
jn <jn@jns-MacBook-Pro.local>
2021-04-23 23:44:07 -0400

    update

--- install_basic_tools.sh
+++ install_basic_tools.sh
@@ -2,7 +2,11 @@
 
 # Install basic tools to simplify the rest of the installation
 
-source ./shell_scripts/homebrew.sh
-source ./shell_scripts/git.sh
-source ./shell_scripts/iterm.sh
-source ./shell_scripts/zsh.sh
+source ./basic_tools/homebrew.sh
+source ./basic_tools/git.sh
+source ./basic_tools/iterm.sh
+source ./basic_tools/zsh.sh
+source ./basic_tools/generate_ssh_keys.sh
+
+# Keep virtualbox at end of install list as it requires system restart
+source ./basic_tools/virtualbox.sh
