85da2cae3cae7b71c25bdbc31f34e8d284015489
jn <jn@jn.local>
2021-04-30 13:35:50 -0400

    .

--- basic_tools.sh
+++ basic_tools.sh
@@ -8,7 +8,7 @@ source ./basic_tools/config_dock.sh
 source ./basic_tools/homebrew.sh
 source ./basic_tools/iterm.sh
 source ./basic_tools/zsh.sh
-# source ./basic_tools/gnupg.sh
+# source ./basic_tools/gnupg.sh # TODO: check if gpg is installed by default
 
 # Keep virtualbox at end of install list as it requires system restart
 source ./basic_tools/virtualbox.sh
---
+++ yubikey.sh
@@ -0,0 +1,10 @@
+
+brew install ykman
+
+brew install yubikey-agent
+
+brew install ykclient
+
+brew install yubico-piv-tool
+brew install pam_yubico
+brew install ykpers
