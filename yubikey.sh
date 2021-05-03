b868ee6df5f0f9276a1cb780491ef4c3e162b3e0
jn <jn@computer.local>
2021-05-03 19:48:45 -0400

    a

--- yubikey.sh
+++ yubikey.sh
@@ -7,7 +7,3 @@ brew install ykclient
 brew install yubico-piv-tool
 brew install pam_yubico
 brew install ykpers
-
-# Tool for generating google-cloud SA certificates
-brew install swig # This is a dependency of google-yubikey
-pip3 install google-yubikey
