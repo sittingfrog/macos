cb45114eb44fcdd9a0daa1e6c4f34ed7cf509e9d
jn <jn@jns-MacBook-Pro.local>
2021-05-06 01:16:02 -0400

    a

--- basic_tools/ssh.sh
+++ basic_tools/ssh.sh
@@ -3,6 +3,6 @@
 # Configure SSH
 mkdir -p ~/.ssh && chmod 700 ~/.ssh
 ln -s "$HOME/code/macos/ssh/config" ~/.ssh/config
-chmod 600 ~/.ssh/config
+chmod 644 ~/.ssh/config
 touch ~/.ssh/known_hosts
 chmod 600 ~/.ssh/known_hosts
