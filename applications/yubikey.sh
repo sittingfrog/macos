5756df2bf8aa70a55537294d43ca3e749df9f19c
jn <jn@jns-MacBook-Pro.local>
2021-05-05 23:43:37 -0400

    a

--- applications/yubikey.sh
+++ applications/yubikey.sh
@@ -6,6 +6,6 @@ brew install ykclient
 brew install pam_yubico
 brew install ykpers
 
-brew install opensc # not necessary but handy for smartcards
+# brew install opensc # not necessary but handy for smartcards
 brew install yubikey-agent
 brew services start yubikey-agent # adds agent to startup list
