18d1b3bca8c647642652eb5d568d1e4d9d304a60
jn <jn@jns-MacBook-Pro.local>
2021-05-30 00:10:09 -0400

    .

--- applications/docker.sh
+++ applications/docker.sh
@@ -5,8 +5,8 @@ brew install docker
 brew install docker-compose
 docker-machine create --driver virtualbox --virtualbox-cpu-count "4" --virtualbox-memory "16384" --virtualbox-disk-size "100000" default
 docker-machine stop default
-echo "Enabling USB functionality on default..."
-vboxmanage modifyvm default --usb on
+# echo "Enabling USB functionality on default..."
+# vboxmanage modifyvm default --usb on
 # vboxmanage usbfilter add 0 --target default --name <name> --vendorid <vid> --productid <pid>
 docker-machine start default
 eval $(docker-machine env default)
