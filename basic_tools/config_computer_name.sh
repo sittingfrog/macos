7abf7d44e17effa8f4d38e7126f5c316c74f777f
jn <jn@jns-MacBook-Pro.local>
2021-05-30 17:13:38 -0400

    d

--- basic_tools/config_computer_name.sh
+++ basic_tools/config_computer_name.sh
@@ -4,8 +4,8 @@ echo '\n\n\n*** STOP *******************************************'
 echo 'Enter a name for your computer that will appear on public networks'
 echo 'Warning: Avoid spaces and special characters'
 echo 'Warning: Avoid using personal information in the name'
-echo 'Enter name (Leave blank to use "computer"):'; read cname;
+echo 'Enter name (Leave blank to use "mbp"):'; read cname;
 if [ -z "$cname" ]; then
-	cname="computer"
+	cname="mbp"
 fi
 defaults write com.apple.rapport familySyncedName -string "$cname"
