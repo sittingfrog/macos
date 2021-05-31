6b898dd3a596b594ad1a7b62e7ba33f8955a47e1
jn <jn@jns-MacBook-Pro.local>
2021-05-30 22:25:55 -0400

    a

---
+++ applications/nightlight.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# This is an application that allows CLI control of nightshift because apple currently doesnt provide this.
+# https://github.com/smudge/nightlight
+brew install smudge/smudge/nightlight
