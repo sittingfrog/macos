b68ceaa71da63ccc22577c300ba7812b8b9455e7
sittingfrog <jvj.norton@gmail.com>
2022-04-22 20:30:01 -0700

    Update config_dock.sh

--- basic_tools/config_dock.sh
+++ basic_tools/config_dock.sh
@@ -4,3 +4,4 @@ defaults write com.apple.dock show-recents -int 0
 defaults write com.apple.dock autohide -int 0
 defaults write com.apple.dock tilesize -int 32
 defaults delete com.apple.dock persistent-apps
+killall Dock
