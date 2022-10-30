d1b65306b512071af91b7aed9c3cbdaca393ecc1
sittingfrog <jvj.norton@gmail.com>
2022-10-29 17:42:14 -0700

    Update applications.sh

--- applications.sh
+++ applications.sh
@@ -4,7 +4,8 @@
 
 source ./applications/sublime.sh
 source ./applications/chrome.sh
-source ./applications/docker.sh
+# source ./applications/docker.sh
+source ./applications/podman.sh
 source ./applications/yubikey.sh
 source ./applications/trezor.sh
 source ./applications/nightlight.sh
